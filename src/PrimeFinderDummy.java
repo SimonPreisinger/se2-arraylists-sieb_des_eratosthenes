18d9429bfe61d4a2a7e0b1f95619e94c39d0f5a9
SimonPreisinger <simonpreisinger@live.de>
2018-05-07 13:50:51 +0200

    working sieb des Eratoshtenes

--- src/PrimeFinderDummy.java
+++ src/PrimeFinderDummy.java
@@ -2,12 +2,29 @@ import java.util.ArrayList;
 
 public class PrimeFinderDummy implements PrimeFinder {
 
-
-
     @Override
     public ArrayList<Integer> findPrimes(Integer upto) {
-        return null;
+        ArrayList<Integer> n = new ArrayList<>(); // Erzeuge die Liste n für alle Zahlen
+        ArrayList<Integer> p = new ArrayList<>(); // Erzeuge die Liste p für Primzahlen
+
+        for (int i = 2; i <= upto; i++) // Fülle n mit den Zahlen 2 bis upto
+        {
+            n.add(i);
+        }
+
+        while (!n.isEmpty()) { // Wiederhole bis n leer ist ...
+            Integer z = n.get(0); // Lies das erste Element z aus n. (z ist eine Primzahl.)
+            p.add(z); // Füge z an p an
+            int factor = 1;
+            int value = factor * z;
+            while (value <= upto) {
+                if (n.remove((Integer)value)) {
+                    n.remove((Integer)value); // Lösche z und alle Vielfachen aus n
+                }
+                value = z * factor++;
+            }
+
+        }
+        return p;
     }
-
-
 }
--- src/PrimeFinderMain.java
+++ src/PrimeFinderMain.java
@@ -1,32 +1,16 @@
 import java.util.ArrayList;
 
-public class PrimeFinderMain implements PrimeFinder {
+public class PrimeFinderMain {
 
     public static void main(String... args) {
+        PrimeFinderDummy primeFinderDummy = new PrimeFinderDummy();
+        ArrayList<Integer> primes = primeFinderDummy.findPrimes(1000);
+        for (Integer prime : primes
+             ) {
+            System.out.println(prime + ", ");
+        }
 
     }
 
-    @Override
-    public ArrayList<Integer> findPrimes(Integer upto) {
-        ArrayList<Integer> n = new ArrayList<>(); // Erzeuge die Liste n für alle Zahlen
-        ArrayList<Integer> p = new ArrayList<>(); // Erzeuge die Liste p für Primzahlen
 
-
-        for (int i = 2; i < upto; i++) // Fülle n mit den Zahlen 2 bis upto
-        {
-            n.add(i);
-        }
-
-        while (n.size() > 0) { // Wiederhole bis n leer ist ...
-
-            Integer z = n.get(0); // Lies das erste Element z aus n. (z ist eine Primzahl.)
-            p.add(z); // Füge z an p an
-            n.remove(z); // Lösche z und alle Vielfachen aus n
-            n.remove(z * z);
-
-
-        }
-
-        return p;
-    }
 }
